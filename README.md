9c78f1c7c596459bd626e18247ec69b45255148d
Shawn Brunsting <sjbrunst@users.noreply.github.com>
2015-06-04 21:27:26 -0400

    Documentation and style changes

--- README.md
+++ README.md
@@ -2,59 +2,64 @@
 This repository contains my submission for the
 [Sortable coding challenge](http://sortable.com/challenge/).
 
-# Quick Start Instructions (Linux):
+# Quick Start Instructions (Linux)
 
-1. `cd` into the repo's root directory
-2. Download a copy of Spark by executing
+1. Clone this repository with `git clone https://github.com/sjbrunst/sortable.git`
+2. `cd sortable` to move into the project's root directory
+3. Download a copy of Spark by executing
 `wget http://d3kbcqa49mib13.cloudfront.net/spark-1.3.1-bin-hadoop2.6.tgz`
-2. Exectue `tar -xf ./spark-1.3.1-bin-hadoop2.6.tgz`
-3. Run `./run.sh`
-4. Results will be saved in results.txt.
+4. Exectue `tar -xf ./spark-1.3.1-bin-hadoop2.6.tgz`
+5. Run `./run.sh`
+6. Results will be saved in results.txt.
 
-# Compiling
+# More Detailed Run Instructions
+
+## Compiling
 
 I included a compiled jar of my program, but if you want to compile it yourself you need sbt. Since
-Sortable's website says that they use Scala, I assume you already have it installed. If not, you
+Sortable's website says that they use Scala, I assume you already have sbt installed. If not, you
 can get sbt [here](http://www.scala-sbt.org/download.html).
 
-To compile my program execute `sbt package` from the root directory of this repository. This will
+To compile my program execute `sbt package` from the root directory of this project. This will
 update the jar file in target/scala-2.10.
 
-# Running
+## Running
 
-Running this program requires Spark. If you do not have Spark you can download a recent copy
-[here](http://spark.apache.org/downloads.html). Set the `sparkPath` configuration option in run.sh
-to be the path to Spark's home folder.
+Running this program requires Spark. If you do not have Spark already you can download a recent
+copy [here](http://spark.apache.org/downloads.html). Set the `sparkPath` configuration option in
+run.sh to be the path to Spark's home folder.
 
-After `sparkPath` is set, you can run my program with run.sh. Other input and output configuration
-options can be modified in run.sh.
+After `sparkPath` is set, you can run my program by executing run.sh. Other input and output
+configuration options, such as changing the input files, can be modified in run.sh.
 
-# Spark
+# Other Information
+
+## Spark
 
 I chose to complete this coding challenge using Apache Spark. This choice was made for a few
 reasons:
 
-* Spark will scale up nicely for problems with more input data. While this coding challenge may not
-require significant computing resources for the size of the input data, it is still good to write
-code that is scalable.
+* Spark will scale up nicely for problems with more input data. While the size of this coding
+challenge may not require significant computing resources, it is still good to write code that is
+scalable.
 * The Spark SQL library makes it easy to read and write the JSON files for this challenge.
 * I am currently using Spark for my Masters research. My familiarity with Spark will minimize the
 amount of time required to create a good submission for this challenge.
 
-# Algorithm
+## Algorithm
 
 My algorithm generates all possible `(Product,Listing)` pairs, then filters them down to pairs that
 look like a valid match. Then, to ensure that each listing is matched to at most one product, all
-possible products are found for one listing and the best product is selected.
+remaining products are found for each listing and the best products are selected.
 
 Listings can have inconsistent capitalization (CANON, Canon, etc.) so all strings that will be
-used for comparisons are first converted to lower case.
+compared as part of the algorithm are first converted to lower case.
 
 Furthermore, products and listings have their strings split into words by splitting on spaces,
 hypens, and several common punctuation marks. This ensures that a string such as `AB-123` would be
 considered identical to `AB 123`.
 
-## Filtering
+### Filtering
 
 The first step in the filtering is to check the manufacturer. `(Product,Listing)` pairs are only
 retained if the product and the listing's manufacturers share at least one word. This ensures that
@@ -62,8 +67,8 @@ a manufacturer such as "Konica Minolta" can be matched to listings with just "Ko
 "Minolta". It also ensures that a manufacturer such as "Canon" can be matched to a listing from
 "Canon Canada".
 
-The next step is to check the family if the product has that field. I ensure that all words in the
-"family" field of the product appear in the listing.
+The next step is to check the family (if the product has that field). I ensure that all words in
+the family field of the product appear in the listing.
 
 Finally, I check the model. This is the trickiest part. For example, the
 Canon\_PowerShot\_SX130\_IS may have "SX130IS," "SX130 IS," or "SX 130 IS" in the listing. I want
@@ -72,23 +77,23 @@ model number 900, I do not want to match listings with model numbers 5900, 7900,
 is necessary to look at groups of words, rather than looking for "900" as a substring. My algorithm
 searches groups of up to three adjacent words for the model number.
 
-## Finding the best product for a listing
+### Finding the best product for a listing
 
 After all the filtering above, one listing may be matched to multiple products. If any products
-have a non-empty family field then I only consider those products. I then choose the product that
-had more words from the manufacturer in the title.
+have a non-empty family field then I only keep those products. I then choose the product that had
+the most words from the manufacturer in the title.
 
-# Known Issues
+## Known Issues
 
 The challenge says "A single price listing may match at most one product." Listings in the input
 file are not unique (example:
 `{"title":"SAMSUNG PL200 - rouge","manufacturer":"Samsung","currency":"EUR","price":"170.99"}`
 appears twice), but my algorithm enforces the challenge's constraint by ensuring that each unique
 listing is only used once. If it is desired to distiguish between identical listings in the input
-file, then they could be given an additional field with a unique id.
+file, then they can be given an additional field with a unique id.
 
 The given list of products all appear to be cameras, but the listings include items such as "Canon
-LP-E6 Battery for..." To avoid false positives for these listings, I remove all listings that
-contain the words "for," "pour," or "für". This results in some missed matches, but this is
-outweighed by the number of false positives that are avoided. This only works for English, French,
-and German listings.
+LP-E6 Battery for Canon EOS 5D Mark II 7D LPE6" To avoid false positives for these listings, I
+remove all listings that contain the words "for," "pour," or "für". This results in some missed
+matches, but this is outweighed by the number of false positives that are avoided. This technique 
+only works for English, French, and German listings.
--- src/main/scala/SortableChallenge.scala
+++ src/main/scala/SortableChallenge.scala
@@ -13,61 +13,63 @@ object SortableChallenge {
   var productsPath = "products.txt"
   var resultsPath = "results"
 
+  /* This class is commented out since it is not actually necessary
   case class Product(
-        product_name: String,   // A unique id for the product
-        manufacturer: String,
-        family: String,         // optional grouping of products
-        model: String
-        // the announced-date is not useful for this algorithm
-    )
+    product_name: String,   // A unique id for the product
+    manufacturer: String,
+    family: String,         // optional grouping of products
+    model: String
+    // the announced-date is not useful for this algorithm
+  )
+  */
 
   // Product with the manufacturer and family split into an array of words:
   case class SplitProduct(
-        product_name: String,   // A unique id for the product
-        manufacturer: Array[String],
-        family: Array[String],         // optional grouping of products
-        model: String
-    )
+    product_name: String,   // A unique id for the product
+    manufacturer: Array[String],
+    family: Array[String],  // optional grouping of products
+    model: String
+  )
 
   case class Listing(
-      title: String,        // description of product for sale
-      manufacturer: String, // who manufactures the product for sale
-      currency: String,     // currency code, e.g. USD, CAD, GBP, etc.
-      price: String         // price, e.g. 19.99, 100.00
-    )
+    title: String,        // description of product for sale
+    manufacturer: String, // who manufactures the product for sale
+    currency: String,     // currency code, e.g. USD, CAD, GBP, etc.
+    price: String         // price, e.g. 19.99, 100.00
+  )
 
   // Listing with the title and manufacturer split into an array of words:
   case class SplitListing(
-      title: Array[String],        // description of product for sale
-      manufacturer: Array[String]  // who manufactures the product for sale
-    )
+    title: Array[String],        // description of product for sale
+    manufacturer: Array[String]  // who manufactures the product for sale
+  )
 
   case class Result(
-      product_name: String,
-      listings: Array[Listing]
-    )
+    product_name: String,
+    listings: Array[Listing]
+  )
 
   def processArgs(list: List[String]): Unit = {
     list match {
       case Nil => return
       case "--listings" :: value :: tail => {
-          listingsPath = value
-          processArgs(tail)
-        }
+        listingsPath = value
+        processArgs(tail)
+      }
       case "--products" :: value :: tail => {
-          productsPath = value
-          processArgs(tail)
-        }
+        productsPath = value
+        processArgs(tail)
+      }
       case "--results" :: value :: tail => {
-          resultsPath = value
-          processArgs(tail)
-        }
+        resultsPath = value
+        processArgs(tail)
+      }
       case string :: tail => {
-          println("Unknown option: " + string)
-          System.exit(1)
-        }
+        println("Unknown option: " + string)
+        System.exit(1)
       }
     }
+  }
 
   def main(args: Array[String]) {
 
@@ -78,77 +80,106 @@ object SortableChallenge {
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
+    // All Listings from the input file:
     val listings = sqlContext.jsonFile(listingsPath)
                              .select("title","manufacturer","currency","price")
                              .map(row => Listing(row.getString(0),
                                                  row.getString(1),
                                                  row.getString(2),
                                                  row.getString(3)))
+
+    // (SplitListing,Listing) pairs for listings we wish to match:
     val splitListings = listings.distinct() // without ids, listings are not unique
                                             // save time later by removing duplicates now
-                                // avoid "battery for" false positives:
+                                // avoid "battery for" or "lens for" false positives:
                                 .filter(sl => !(sl.title.contains("for") ||
                                                 sl.title.contains("pour") ||
                                                 sl.title.contains("für")))
-                                .map(l => (SplitListing(l.title.toLowerCase().split(splitCharacters),
-                                                        l.manufacturer.toLowerCase().split(splitCharacters)),
+                                .map(l => (SplitListing(l.title
+                                                         .toLowerCase()
+                                                         .split(splitCharacters),
+                                                        l.manufacturer
+                                                         .toLowerCase()
+                                                         .split(splitCharacters)),
                                            l)) // keep the original listing for output at the end
                                 .cache()
+
+    // SplitProducts for all products in the input file:
     val splitProducts = sqlContext.jsonFile(productsPath)
                                   .select("product_name","manufacturer","family","model")
                                   .map(row => SplitProduct(row.getString(0),
-                                                 row.getString(1).toLowerCase().split(" "),
-                                                 // "family" is optional:
-                                                 if (row.isNullAt(2)) {Array()}
-                                                 else {row.getString(2)
-                                                          .toLowerCase()
-                                                          .split(splitCharacters)},
-                                                 row.getString(3)
-                                                    .toLowerCase()
-                                                    // remove any spaces, etc:
-                                                    .split(splitCharacters)
-                                                    .reduce(_ + _)))
+                                                           row.getString(1)
+                                                              .toLowerCase()
+                                                              .split(" "),
+                                                           // "family" is optional:
+                                                           if (row.isNullAt(2)) {Array()}
+                                                           else {row.getString(2)
+                                                                    .toLowerCase()
+                                                                    .split(splitCharacters)},
+                                                           row.getString(3)
+                                                              .toLowerCase()
+                                                              // next 2 lines remove spaces, etc:
+                                                              .split(splitCharacters)
+                                                              .reduce(_ + _)))
                                   .cache()
 
+    // Possible matches are stored as (SplitProduct,(SplitListing,Listing)) pairs:
     val allPairs = splitProducts.cartesian(splitListings)
-    // now we have all possible (product,listing) pairs, and we want to reduce them
+
+    // We now have all possible matches, and we want to filter them to ones that make sense.
 
     val filtered = allPairs.filter{case (sp,(sl,l)) => sl.manufacturer
-                                                         .intersect(sp.manufacturer).length > 0}
+                                                         .intersect(sp.manufacturer)
+                                                         .length > 0}
                            .filter{case (sp,(sl,l)) => (sp.family.length == 0) ||
-                                                       (sl.title.intersect(sp.family).length == sp.family.length)}
+                                                       (sl.title
+                                                          .intersect(sp.family)
+                                                          .length == sp.family.length)}
                            .filter{case (sp,(sl,l)) => (sl.title.contains(sp.model) ||
-                                                       (sl.title.sliding(2).map(_.reduce(_ + _)).contains(sp.model)) ||
-                                                       (sl.title.sliding(3).map(_.reduce(_ + _)).contains(sp.model))) }
+                                                       (sl.title
+                                                          .sliding(2)
+                                                          .map(_.reduce(_ + _))
+                                                          .contains(sp.model)) ||
+                                                       (sl.title
+                                                          .sliding(3)
+                                                          .map(_.reduce(_ + _))
+                                                          .contains(sp.model))) }
 
     // So far one listing could be matched to multiple products.
     // uniqueListings below matches each listing to the best product.
 
-    // I use the entire Listing object as the key here
-    // If listings had unique ids then I would use that instead
     val uniqueListings = filtered.map{case (sp,(sl,l)) => (l,(sl,sp))}
+                                 // I use the entire Listing object as the key here
+                                 // If listings had unique ids then I would use that instead
                                  .groupByKey()
+                                 // Now we have (Listing,List((SplitListing,SplitProduct))).
+                                 // The SplitListing is the same for every pair in the list, so
+                                 // take the first one:
                                  .mapValues(slspList => (slspList.head._1, slspList.map(_._2)))
+                                 // Now we have (Listing,(SplitListing,List(SplitProduct)))
                                  .mapValues{case (sl, spList) => {
                                      // if there are products with non-empty family fields:
                                      if (spList.filter(_.family.length > 0).size > 0) {
                                        // only use products that had to match a family too:
-                                       spList.filter(_.family.length > 0) 
-                                          .maxBy(sp => (sl.title.intersect(sp.manufacturer)).length)
+                                       spList.filter(_.family.length > 0)
+                                             .maxBy(sp => (sl.title
+                                                             .intersect(sp.manufacturer))
+                                                             .length)
                                      } else {
-                                       spList.maxBy(sp => (sl.title.intersect(sp.manufacturer)).length)}
+                                       spList.maxBy(sp => (sl.title
+                                                             .intersect(sp.manufacturer))
+                                                             .length)}
                                      }
                                    }
 
+    // uniqueListings now has (Listing,SplitProduct) pairs
+
     val results = uniqueListings.map{case (l, sp) => (sp.product_name, l)}
                                 .groupByKey()
-                                .map{case (product_name,lList) => Result(product_name,lList.toArray)}
+                                .map{case (product_name,lList) => Result(product_name,
+                                                                         lList.toArray)}
 
     results.toDF().toJSON.saveAsTextFile(resultsPath)
-
-    // print the unmatched listings for debug
-    //val unmatched = listings.distinct().subtract(results.flatMap(r => r.listings))
-    //unmatched.toDF().toJSON.saveAsTextFile("unmatched")
 
     sc.stop()
   }
